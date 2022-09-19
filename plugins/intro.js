b5a828d690ecd3f8561c69666b53b41fd747c037
Lolllipoppp <102265928+Lolllipoppp@users.noreply.github.com>
2022-09-19 17:21:04 +0700

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -38,7 +38,7 @@ let krtu = `0ཻུ۪۪ꦽꦼ̷⸙‹•═════════════�
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, krtu, wm,'MENU','.menu', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/tokoriku_",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
     title: 'RIKU GANTENG',
