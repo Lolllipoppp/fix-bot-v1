f2f99f778a6ba007559f538865b4858e4aef31e7
Lolllipoppp <102265928+Lolllipoppp@users.noreply.github.com>
2022-09-19 17:23:25 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -15,10 +15,10 @@ wa.me/6287873985625`
 let wibu = `https://api-reysekha.herokuapp.com/api/random/cosplay?apikey=apirey` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'Thankyou','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/_ctzhid",
+    mediaUrl: "https://Instagram.com/tokoriku_",
     mediaType: "VIDEO",
     description: "https://www.instagram.com/p/CevoCg5hG-p/?utm_source=ig_web_copy_link", 
-    title: 'WH-MODS-DEV-V1 MultiDevice',
+    title: 'Riku Bot Multidevice',
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
